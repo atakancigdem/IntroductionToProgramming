3c45d7a80351e7faf2e8527a65858245218c06af
JupiteRus <atakancigdem7@outlook.com>
2021-01-20 23:01:45 +0300

    I Learn C#

---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] Names = new string[] { "Atakan", "Furkan", "Melek" };
+            //Console.WriteLine(Names[0]);
+            //Console.WriteLine(Names[1]);
+            //Console.WriteLine(Names[2]);
+
+            //Names = new string[3];
+            //Names[3] = "Coni";
+            //Console.WriteLine(Names[3]);
+            //Console.WriteLine(Names[0]);
+
+            List<string> Names2 = new List<string> { "Atakan", "Furkan", "Melek" };
+        }
+    }
+}
